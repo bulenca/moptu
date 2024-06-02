8a852f61dcfd9892688c5277d1b4edec1d317d06
bulenca <baczaski@gmail.com>
2024-06-02 22:13:31 +0200

    Configure AndroidManifest and import Picasso to load images

--- app/src/main/java/com/example/moptu/LocationDetail.kt
+++ app/src/main/java/com/example/moptu/LocationDetail.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class LocationDetail : Fragment() {
     override fun onCreateView(
@@ -18,12 +19,16 @@ class LocationDetail : Fragment() {
         val nameTextView: TextView = view.findViewById(R.id.textViewLocationName)
         val descriptionTextView: TextView = view.findViewById(R.id.textViewLocationDescription)
         val detailedDescriptionTextView: TextView = view.findViewById(R.id.textViewDetailedDescription)
+        val imageView: ImageView = view.findViewById(R.id.imageViewLocation)
+
 
 
         arguments?.let {
             nameTextView.text = it.getString("location_name")
             descriptionTextView.text = it.getString("location_description")
             detailedDescriptionTextView.text = it.getString("location_detailed_description")
+            val imageUrl = it.getString("location_image_url")
+            Picasso.get().load(imageUrl).into(imageView)
         }
 
         return view
