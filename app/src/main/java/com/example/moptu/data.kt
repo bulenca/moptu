de9f7150053b397c433dba47f32aee2250df1c87
bulenca <baczaski@gmail.com>
2024-06-02 21:58:06 +0200

    Update Location data class

--- app/src/main/java/com/example/moptu/data.kt
+++ app/src/main/java/com/example/moptu/data.kt
@@ -1,5 +1,7 @@
 package com.example.moptu
 data class Location(
     val name: String,
-    val description: String
+    val description: String,
+    val detailedDescription: String,
+    val imageUrl: String
 )
